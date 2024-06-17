872d759c1d8a432bdcb6be8dd43bd25b59c4bd91
QIWEB <zqijava@163.com>
2024-06-17 14:24:43 +0800

    Update weather_report.py

--- weather_report.py
+++ weather_report.py
@@ -120,14 +120,31 @@ def send_weather(access_token, weather):
 
 def weather_report(this_city):
     # 1.获取access_token
-    access_token = get_access_token()
+    #access_token = get_access_token()
     # 2. 获取天气
     weather = get_weather(this_city)
     print(f"天气信息： {weather}")
     # 3. 发送消息
-    send_weather(access_token, weather)
+    #send_weather(access_token, weather)
+    send_message('0a1831c39831412f94e0142bb5708305','今天上海天气',weather)
+
+import requests
+import json
 
 
+def send_message(token, title, content):
+    url = "http://www.pushplus.plus/send"
+    data = {"token": token, "title": title, "content": content}
+    body = json.dumps(data).encode(encoding="utf-8")
+    headers = {"Content-Type": "application/json"}
+    response = requests.post(url, data=body, headers=headers)
+    # 解析 JSON 数据
+    response_dict = json.loads(response.text)
+    # 删除 "data" 字段
+    if "data" in response_dict:
+        response_dict.pop("data")
+
+    return response_dict
 
 if __name__ == '__main__':
-    weather_report("淄博")
+    weather_report("上海天气")
